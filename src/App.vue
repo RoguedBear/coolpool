3bc4dd7a3bdb466b4e03891ac5dda14f1eed485d
RoguedBear <39442192+RoguedBear@users.noreply.github.com>
2022-08-20 13:57:37 +0530

    first commit, so much to unpack

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <MenuItem json-menu="{}" />
+</template>
+
+<script>
+import MenuItem from "./components/MenuItem.vue";
+export default {
+  name: "App",
+  components: { MenuItem },
+};
+</script>
+
+<style>
+#app {
+  font-family: Oxygen, Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+@font-face {
+  font-family: "Oxygen";
+  src: local("Oxygen"), url("./assets/fonts/Oxygen.ttf") format("truetype");
+}
+</style>
---
+++ src/components/MenuItem.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="border">
+    <div>
+      <ul class="menu-card-top-row">
+        <li>{{ type }}</li>
+        <li>qr_icon</li>
+        <li v-if="availed === false" class="not-availed">{{ code }}</li>
+        <li v-else class="availed">Availed</li>
+      </ul>
+    </div>
+    <div>
+      <span>{{ menuItems }}</span>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "MenuItem",
+  props: ["jsonMenu"],
+  data() {
+    return {
+      menuItems:
+        "lorem ipsum, idli ipsum, aloo ipsum, lorem paneer, spam, spam, spam so much stuff to eat oh wowowoo",
+      availed: false,
+      type: "Breakfast",
+      code: "794835",
+    };
+  },
+};
+</script>
+
+<style>
+.menu-card-top-row {
+  display: flex;
+  justify-content: space-between;
+  padding: 0%;
+}
+ul {
+  list-style: none;
+}
+
+.border {
+  border-radius: 4rem;
+  border-color: purple;
+  border-width: 5vh;
+}
+</style>
