6806f75d6c86cbdcf1a16d56eb97187237f3e5be
RoguedBear <39442192+RoguedBear@users.noreply.github.com>
2022-10-09 14:53:12 +0530

    fix CORS issue by setting up proxy

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,29 @@
 const { defineConfig } = require("@vue/cli-service");
+
+process.env.VUE_APP_API_URL =
+  process.env.USE_PROXY == "true"
+    ? process.env.VUE_APP_API_URL_FOR_PROXY
+    : process.env.VUE_APP_API_URL;
+process.env.VUE_APP_USING_PROXY = process.env.USE_PROXY == "true";
+
 module.exports = defineConfig({
   transpileDependencies: true,
+  devServer:
+    process.env.USE_PROXY == "true"
+      ? {
+          proxy: {
+            "^/rest": {
+              target: process.env.VUE_APP_PROXY_BACKEND_URL,
+              changeOrigin: true,
+
+              onProxyReq: function (request) {
+                request.setHeader(
+                  "origin",
+                  process.env.VUE_APP_PROXY_BACKEND_URL
+                );
+              },
+            },
+          },
+        }
+      : {},
 });
