7d6b260ce83742c5b16641c8dabfb23ee947f0f1
RoguedBear <39442192+RoguedBear@users.noreply.github.com>
2022-10-10 22:45:12 +0530

    add testing, ampha mode dark theme

--- src/App.vue
+++ src/App.vue
@@ -57,7 +57,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #ffffff;
   margin-top: 60px;
 }
 @font-face {
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
+import "./../public/style.css";
 
 createApp(App).mount("#app");
