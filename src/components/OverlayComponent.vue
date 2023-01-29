92162059f389035fa76fda495f4782af5756f9f9
RoguedBear <39442192+RoguedBear@users.noreply.github.com>
2023-01-29 18:43:45 +0530

    convert `QROverlay` into a generic `Overlay` component
    accepts template data using slots

---
+++ src/components/OverlayComponent.vue
@@ -0,0 +1,29 @@
+<template>
+  <div @click="emitToggle" class="overlay" v-if="showQR">
+    <div class="border solid-bg">
+      <h1>{{ title }}</h1>
+      <slot></slot>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "OverlayComponent",
+  props: {
+    title: String,
+    showQR: Boolean,
+  },
+  methods: {
+    emitToggle() {
+      console.log("emmiting toggle");
+      this.$emit("toggleQR", false);
+    },
+  },
+};
+</script>
+<style>
+.solid-bg {
+  background-color: black;
+  align-self: center;
+}
+</style>
