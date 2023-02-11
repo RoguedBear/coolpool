ccd6c5c81f069ba1ea646b008c81227f63fc1754
RoguedBear <39442192+RoguedBear@users.noreply.github.com>
2023-02-11 16:20:38 +0530

    add the ability to view/generate qr code

--- src/utilities.js
+++ src/utilities.js
@@ -17,3 +17,25 @@ export function parseJwt(token) {
 export function isFirefox() {
   return navigator.userAgent.indexOf("Firefox") !== -1;
 }
+
+export let QRFormat = {
+  current_millis: Date.now(),
+  // device: { app_version: "1.92", name: "SM-A315F", os_version: "31" },
+  module: {
+    details: {
+      shop_id: "-1",
+      auto_approval_status: "false",
+      coupon_code: "51329",
+      order_id: "-1",
+    },
+    sub_type: "reminder_coupon_code",
+    type: "reminders",
+  },
+  user: {
+    college_id: 136,
+    id: 234224,
+    name: "Darth Vader",
+    user_type: "student",
+  },
+  version: 2,
+};
